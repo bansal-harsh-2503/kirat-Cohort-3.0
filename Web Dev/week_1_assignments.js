0ac545d457688e63a377e2c05392279632946774
harsh <focusedstudytarget2022@gmail.com>
2024-08-14 20:54:23 +0530

    week 1 assignments

---
+++ Web Dev/week_1_assignments.js
@@ -0,0 +1,13 @@
+// Assignment #1
+function sum(a, b){
+    return a+b;
+} 
+console.log(sum(3, 4));
+console.log(sum("a", 7));
+
+//Assignment #2
+function canVote(age){
+    return age >= 18;
+}
+console.log(canVote(17));
+console.log(canVote(18));
